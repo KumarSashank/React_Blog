38378dc15e193bc1d934d4d44f8d6652a0d132f9
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2023-01-07 22:41:14 +0530

    Designed Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../img/logo.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="container">
-                <div className="logo">Logo</div>
-                <div className="links">Links</div>
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="links">
+                    <Link className='link' to="/?cat=art "><h6>ART</h6></Link>
+                    <Link className='link' to="/?cat=science "><h6>Science</h6></Link>
+                    <Link className='link' to="/?cat=technology "><h6>Technology</h6></Link>
+                    <Link className='link' to="/?cat=design "><h6>Design</h6></Link>
+                    <Link className='link' to="/?cat=food "><h6>Food</h6></Link>
+                    <span>John</span>
+                    <span>Logout</span>
+                    <span className="write">    
+                        <Link className="link" to="/write">Write</Link>
+                    </span>
+                </div>
             </div>
         </div>
     );
