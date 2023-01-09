0d20df7f35547fbb72b89b229d80e05fca43dc66
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2023-01-09 21:09:59 +0530

    Added text editor

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -34,7 +34,7 @@ const Menu = () => {
             <h1>Other posts you may like</h1>
         {posts.map((post) => (
             <div className="post" key={post.id}>
-                <img src={post.img} alt={post.title}/>
+                <img src={post.img}/>
                 <h2>{post.title}</h2>
                 <button>Read more</button>
             </div>
