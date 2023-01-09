619ff2688fa8390ab6ddfde92fca69ffc23c629b
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2023-01-10 00:01:58 +0530

    added side menu in write page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -14,12 +14,54 @@ const Write = () => {
             <div className="content">
                 <input type="text" placeholder='Title' />
                 <div className="editorContainer">
-                <ReactQuill theme="snow" value={value} onChange={setValue} />
+                <ReactQuill theme="snow" className="editor" value={value} onChange={setValue} />
                 </div>
             </div>
             <div className="menu">
-                <div className="item">i1</div>
-                <div className="item">i2</div>
+                <div className="item">
+                    <h1>Publish</h1>
+                    <span>
+                        <b>Status: </b> Draft
+                    </span>
+                    <span>
+                        <b>Visibility: </b> Public
+                    </span>
+                    <input type="file" id="file"/>
+                    <label htmlFor="file">Upload Image</label>
+                    <div className="buttons">
+                        <button>Save as a draft</button>
+                        <button>Update</button>
+                    </div>
+                    </div>
+                <div className="item"> 
+                <h1>Category</h1>
+                <div className="cat">
+                <div className="box">
+                    <input type="radio" id="art" value="art" />
+                    <label htmlFor="art">Art</label>
+                </div>
+                <div className="box">
+                    <input type="radio" id="science" value="science" />
+                    <label htmlFor="science">Science</label>
+                </div>
+                <div className="box">
+                    <input type="radio" id="technology" value="technology" />
+                    <label htmlFor="technology">Technology</label>
+                </div>
+                <div className="box">
+                    <input type="radio" id="cinema" value="cinema" />
+                    <label htmlFor="cinema">Cinema</label>
+                </div>
+                <div className="box">
+                    <input type="radio" id="design" value="design"/>
+                    <label htmlFor="design">Design</label>
+                </div>
+                <div className="box">
+                    <input type="radio" id="food" value="food"/>
+                    <label htmlFor="food">Food</label>
+                </div>
+                </div>
+                </div>
             </div>
         </div>
     );
